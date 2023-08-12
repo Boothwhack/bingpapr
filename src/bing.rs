3d08f8ebf60e579647f0634ffad3b468c9582248
Karoline <boothwhack@users.noreply.github.com>
2023-08-12 12:42:54 +0200

    Separate module for Bing api calls

---
+++ src/bing.rs
@@ -0,0 +1,161 @@
+use std::fmt::{Debug, Formatter};
+use std::io;
+use std::path::{Path, PathBuf};
+use std::str::FromStr;
+use chrono::{DateTime, NaiveDate, NaiveTime, Utc};
+use log::debug;
+use serde::Deserialize;
+use thiserror::Error;
+use tokio::fs::File;
+use tokio::io::AsyncWriteExt;
+use tokio_stream::StreamExt;
+
+#[derive(Default)]
+pub enum Market {
+    DanishDenmark,
+    EnglishGB,
+    #[default]
+    EnglishUS,
+}
+
+impl Debug for Market {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        f.write_str(&self.to_string())
+    }
+}
+
+impl ToString for Market {
+    fn to_string(&self) -> String {
+        match self {
+            Market::DanishDenmark => "da-DK".to_owned(),
+            Market::EnglishGB => "en-GB".to_owned(),
+            Market::EnglishUS => "en-US".to_owned(),
+        }
+    }
+}
+
+#[derive(Debug, Error)]
+#[error("Unknown market: {0}")]
+pub struct UnknownMarket(String);
+
+impl FromStr for Market {
+    type Err = UnknownMarket;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "da-DK" => Ok(Market::DanishDenmark),
+            "en-GB" => Ok(Market::EnglishGB),
+            "en-US" => Ok(Market::EnglishUS),
+            _ => Err(UnknownMarket(s.to_owned())),
+        }
+    }
+}
+
+const BING_IMAGE_API_BASE_URL: &str = "https://www.bing.com/HPImageArchive.aspx";
+const BING_BASE_URL: &str = "https://www.bing.com";
+
+#[derive(Deserialize)]
+struct BingAPIResponse {
+    images: Vec<BingImage>,
+}
+
+#[derive(Deserialize)]
+pub struct BingImage {
+    #[serde(rename = "startdate")]
+    start_date: String,
+    #[serde(rename = "fullstartdate")]
+    full_start_date: String,
+    #[serde(rename = "enddate")]
+    end_date: String,
+    url: String,
+    #[serde(rename = "urlbase")]
+    url_base: String,
+    title: String,
+}
+
+const DATE_FORMAT: &str = "%Y%m%d";
+const TIME_FORMAT: &str = "%H%M";
+
+pub fn parse_bing_date(s: &str) -> Result<DateTime<Utc>, chrono::ParseError> {
+    let (date, time) = NaiveDate::parse_and_remainder(s, &DATE_FORMAT)?;
+    let time = NaiveTime::parse_from_str(time, &TIME_FORMAT)
+        .unwrap_or_else(|_| NaiveTime::from_hms_opt(7, 0, 0).unwrap());
+
+    Ok(date.and_time(time).and_utc())
+}
+
+impl BingImage {
+    pub fn get_image_url(&self) -> String {
+        format!("{}{}_UHD.jpg", BING_BASE_URL, self.url_base)
+    }
+
+    pub fn get_image_file_name(&self) -> String {
+        format!("{}-{}.jpg", self.start_date, self.title)
+    }
+
+    pub fn get_end_date(&self) -> Result<DateTime<Utc>, chrono::ParseError> {
+        parse_bing_date(&self.end_date)
+    }
+}
+
+#[derive(Debug, Error)]
+pub enum ImageOfTheDayError {
+    #[error(transparent)]
+    RequestError(#[from] reqwest::Error),
+    #[error("Bing API did not return any images")]
+    NoImagesFound,
+}
+
+#[derive(Debug, Error)]
+pub enum DownloadImageError {
+    #[error(transparent)]
+    RequestError(#[from] reqwest::Error),
+    #[error("Failed to write image to {0:?}: {1}")]
+    IoError(PathBuf, #[source] io::Error),
+}
+
+pub struct Bing {
+    client: reqwest::Client,
+}
+
+impl Bing {
+    pub fn new() -> Bing {
+        Bing {
+            client: reqwest::Client::new(),
+        }
+    }
+
+    pub async fn image_of_the_day(&self) -> Result<BingImage, ImageOfTheDayError> {
+        let mut response = self
+            .client
+            .get(BING_IMAGE_API_BASE_URL)
+            .query(&[
+                ("format", "js"),
+                ("idx", "0"),
+                ("n", "1"),
+            ])
+            .send()
+            .await?
+            .json::<BingAPIResponse>()
+            .await?;
+
+        let mut images = response.images.drain(..);
+        images.next().ok_or(ImageOfTheDayError::NoImagesFound)
+    }
+
+    pub async fn download_image(&self, image: &BingImage, path: &Path) -> Result<(), DownloadImageError> {
+        let url = image.get_image_url();
+
+        debug!("Downloading image from {} into {}", url, path.display());
+
+        let response = self.client.get(&url).send().await?;
+        let mut file = File::create(&path)
+            .await
+            .map_err(|err| DownloadImageError::IoError(path.to_owned(), err))?;
+        let mut bytes = response.bytes_stream();
+        while let Some(Ok(item)) = bytes.next().await {
+            file.write_all(&item).await.map_err(|err| DownloadImageError::IoError(path.to_owned(), err))?;
+        }
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,36 @@
+mod bing;
 mod hyprpaper;
 
-use std::fmt::{Debug, Formatter};
+use std::fmt::Debug;
 use std::path::{Path, PathBuf};
 use std::{io, process};
-use std::str::{Chars, FromStr};
+use std::str::FromStr;
 use std::sync::Arc;
-use chrono::{DateTime, Duration, NaiveDate, Utc};
+use chrono::{DateTime, Duration, Utc};
 use hyprland::event_listener::EventListener;
 use hyprland::prelude::*;
 use log::{debug, error, warn};
-use reqwest::{Client, Error};
-use serde::Deserialize;
 use thiserror::Error;
-use tokio::fs::File;
-use tokio::io::AsyncWriteExt;
 use std::sync::Mutex;
 use tokio::task::LocalSet;
-use tokio_stream::StreamExt;
 use tokio_walltime::sleep_until;
+use crate::bing::{Bing, Market};
 use crate::hyprpaper::Hyprpaper;
 
-#[derive(Default)]
-pub enum Market {
-    DanishDenmark,
-    EnglishGB,
-    #[default]
-    EnglishUS,
-}
-
-impl Debug for Market {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        f.write_str(&self.to_string())
-    }
-}
-
-impl ToString for Market {
-    fn to_string(&self) -> String {
-        match self {
-            Market::DanishDenmark => "da-DK".to_owned(),
-            Market::EnglishGB => "en-GB".to_owned(),
-            Market::EnglishUS => "en-US".to_owned(),
-        }
-    }
-}
-
 #[derive(Debug, Error)]
-#[error("Unknown market: {0}")]
-pub struct UnknownMarket(String);
-
-impl FromStr for Market {
-    type Err = UnknownMarket;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s {
-            "da-DK" => Ok(Market::DanishDenmark),
-            "en-GB" => Ok(Market::EnglishGB),
-            "en-US" => Ok(Market::EnglishUS),
-            _ => Err(UnknownMarket(s.to_owned())),
-        }
-    }
+enum ApplyWallpaperError {
+    #[error(transparent)]
+    HyprError(#[from] hyprland::shared::HyprError),
+    #[error(transparent)]
+    IoError(#[from] io::Error),
+    #[error("hyprctl exited with code {0:?}: {1}")]
+    ExecuteHyprCtlError(Option<i32>, String),
+    #[error("hyprctl exited with code {0:?} and parsing output as UTF-8 failed: {1}")]
+    ParseUtf8Error(Option<i32>, #[source] std::string::FromUtf8Error),
 }
 
 #[derive(Debug, Default)]
-struct Configuration {
+pub struct Configuration {
     pub market: Market,
     /// Alternative directory to store downloaded wallpaper files. Defaults to
     /// '$XDG_PICTURES_DIR/Bing Wallpapers' if available, otherwise the configuration directory.
@@ -93,160 +61,8 @@ impl Configuration {
     }
 }
 
-const BING_IMAGE_API_BASE_URL: &str = "https://www.bing.com/HPImageArchive.aspx";
-const BING_BASE_URL: &str = "https://www.bing.com";
-
-#[derive(Deserialize)]
-struct BingAPIResponse {
-    images: Vec<BingImage>,
-}
-
-#[derive(Deserialize)]
-struct BingImage {
-    #[serde(rename = "startdate")]
-    start_date: String,
-    #[serde(rename = "fullstartdate")]
-    full_start_date: String,
-    #[serde(rename = "enddate")]
-    end_date: String,
-    url: String,
-    #[serde(rename = "urlbase")]
-    url_base: String,
-    title: String,
-}
-
-impl BingImage {
-    fn get_image_url(&self) -> String {
-        format!("{}{}_UHD.jpg", BING_BASE_URL, self.url_base)
-    }
-
-    fn get_image_file_name(&self) -> String {
-        format!("{}-{}.jpg", self.start_date, self.title)
-    }
-}
-
-#[derive(Debug, Error)]
-enum QueryImageOfTheDayError {
-    #[error(transparent)]
-    RequestError(#[from] Error),
-    #[error("Bing API did not return any images")]
-    NoImagesFound,
-}
-
-async fn query_image_of_the_day(client: &Client) -> Result<BingImage, QueryImageOfTheDayError> {
-    let response = client.get(BING_IMAGE_API_BASE_URL)
-        .query(&[
-            ("format", "js"),
-            ("idx", "0"),
-            ("n", "1")
-        ])
-        .send().await?;
-    let mut response = response.json::<BingAPIResponse>().await?;
-
-    let mut images = response.images.drain(..);
-    images.next().ok_or(QueryImageOfTheDayError::NoImagesFound)
-}
-
-#[derive(Debug, Error)]
-enum DownloadImageError {
-    #[error(transparent)]
-    RequestError(#[from] reqwest::Error),
-    #[error("Failed to write image to {0:?}: {1}")]
-    IoError(PathBuf, #[source] io::Error),
-}
-
-async fn download_image(image: &BingImage, path: &Path) -> Result<(), DownloadImageError> {
-    let url = image.get_image_url();
-    debug!("Downloading image from {} into {}", url, path.display());
-
-    let response = reqwest::get(&url).await?;
-    let mut file = File::create(path).await.map_err(|err| DownloadImageError::IoError(path.to_owned(), err))?;
-    let mut bytes = response.bytes_stream();
-    while let Some(Ok(item)) = bytes.next().await {
-        file.write_all(&item).await.map_err(|err| DownloadImageError::IoError(path.to_owned(), err))?;
-    }
-    Ok(())
-}
-
-#[derive(Debug, Error)]
-enum ApplyWallpaperError {
-    #[error(transparent)]
-    HyprError(#[from] hyprland::shared::HyprError),
-    #[error(transparent)]
-    IoError(#[from] io::Error),
-    #[error("hyprctl exited with code {0:?}: {1}")]
-    ExecuteHyprCtlError(Option<i32>, String),
-    #[error("hyprctl exited with code {0:?} and parsing output as UTF-8 failed: {1}")]
-    ParseUtf8Error(Option<i32>, #[source] std::string::FromUtf8Error),
-}
-
-fn execute_hyprctl_hyprpaper(command: &str, argument: &str) -> Result<(), ApplyWallpaperError> {
-    debug!("Executing: hyprctl hyprpaper {} {}", command, argument);
-    let output = process::Command::new("hyprctl")
-        .arg("hyprpaper")
-        .arg(command)
-        .arg(&format!("\"{}\"", argument))
-        .output()?;
-    if !output.status.success() {
-        return Err(ApplyWallpaperError::ExecuteHyprCtlError(
-            output.status.code(),
-            String::from_utf8(output.stderr)
-                .map_err(|err| ApplyWallpaperError::ParseUtf8Error(output.status.code(), err))?,
-        ));
-    }
-
-    if let Ok(stdout) = String::from_utf8(output.stdout) {
-        debug!("Output stdout:\n{}", stdout);
-    }
-    if let Ok(stderr) = String::from_utf8(output.stderr) {
-        debug!("Output stderr:\n{}", stderr);
-    }
-
-    Ok(())
-}
-
-#[derive(Debug, Error)]
-enum ParseBingDateError {
-    #[error("Unexpected end of string")]
-    EndOfString,
-    #[error(transparent)]
-    ParseIntError(#[from] std::num::ParseIntError),
-    #[error("Invalid date")]
-    InvalidDate,
-}
-
-fn parse_date(date: &str) -> Result<DateTime<Utc>, ParseBingDateError> {
-    let mut chars = date.chars();
-
-    fn next_string(chars: &mut Chars, len: u32) -> Option<String> {
-        let mut string = String::with_capacity(len as usize);
-        for _ in 0..len {
-            string.push(chars.next()?);
-        }
-        Some(string)
-    }
-
-    let year = next_string(&mut chars, 4).ok_or(ParseBingDateError::EndOfString)?.parse::<i32>()?;
-    let month = next_string(&mut chars, 2).ok_or(ParseBingDateError::EndOfString)?.parse::<u32>()?;
-    let day = next_string(&mut chars, 2).ok_or(ParseBingDateError::EndOfString)?.parse::<u32>()?;
-
-    let hour = match next_string(&mut chars, 2) {
-        Some(hour) => hour.parse::<u32>()?,
-        // default to 7am
-        None => 7,
-    };
-    let min = match next_string(&mut chars, 2) {
-        Some(min) => min.parse::<u32>()?,
-        None => 0,
-    };
-
-    Ok(NaiveDate::from_ymd_opt(year, month, day).ok_or(ParseBingDateError::InvalidDate)?
-        .and_hms_opt(hour, min, 0).ok_or(ParseBingDateError::InvalidDate)?
-        .and_utc())
-}
-
 struct BingWallpaper {
-    client: Client,
+    bing: Bing,
     hyprpaper: Hyprpaper,
     configuration: Configuration,
     last_picture: Mutex<Option<PathBuf>>,
@@ -269,7 +85,7 @@ impl BingWallpaper {
         let mut last_picture = self.last_picture.lock().unwrap();
 
         debug!("Updating wallpaper");
-        let image = match query_image_of_the_day(&self.client).await {
+        let image = match self.bing.image_of_the_day().await {
             Ok(image) => image,
             Err(error) => {
                 error!("Failed to query image of the day: {}, retrying in an hour.", error);
@@ -286,7 +102,7 @@ impl BingWallpaper {
         } else {
             // check if picture is already downloaded
             if !picture_path.exists() {
-                if let Err(error) = download_image(&image, &picture_path).await {
+                if let Err(error) = self.bing.download_image(&image, &picture_path).await {
                     error!("Failed to download image: {}, retrying in an hour.", error);
                     return DateTime::from(Utc::now() + Duration::hours(1));
                 }
@@ -317,7 +133,7 @@ impl BingWallpaper {
             *last_picture = Some(picture_path);
         }
 
-        match parse_date(&image.end_date) {
+        match image.get_end_date() {
             Ok(end_date) if end_date < Utc::now() => {
                 warn!("Bing returned end date in the past, assuming 24 hours from now");
                 Utc::now() + Duration::hours(24)
@@ -350,7 +166,7 @@ impl BingWallpaper {
 async fn main() {
     env_logger::builder().target(env_logger::Target::Stdout).init();
 
-    let client = Client::new();
+    let bing = Bing::new();
     let configuration = Configuration::default();
     let hyprpaper = Hyprpaper::new().expect("failed to connect to hyprpaper IPC");
 
@@ -359,7 +175,7 @@ async fn main() {
     // TODO: Ensure copyright tag is properly set on image file
 
     let bing = BingWallpaper {
-        client,
+        bing,
         hyprpaper,
         configuration,
         last_picture: Mutex::new(None),
